fcdbe518795805b00e563c1d078b3532723831f7
epaaso <netopaas@gmail.com>
2020-08-06 13:28:05 -0500

    serializer

---
+++ tree.py
@@ -0,0 +1,30 @@
+import json
+from json import JSONEncoder
+
+
+class TreeNode:
+
+    def __init__(self, value=1, parent=None):
+        self.value = value
+        self.parent = parent
+        self.children = []
+
+        if parent is not None:
+            self.parent.children.append(self)
+
+
+# subclass JSONEncoder
+class TreeEncoder(JSONEncoder):
+    def default(self, o):
+        attrs = o.__dict__
+        if attrs['parent'] is not None:
+            attrs['parent'] = o.parent.value
+        return attrs
+
+
+a = TreeNode()
+
+b = TreeNode(parent=a)
+
+a_json = json.dumps(a, indent=4, cls=TreeEncoder)
+a_json
